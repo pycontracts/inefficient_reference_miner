1cfcb828d9f195d220abc42ae800b5287e98ccfb
Build Server <git@bitcoinpy.io>
2018-12-24 22:40:53 +1000

    Many examples in the code

--- miner.py
+++ miner.py
@@ -54,7 +54,7 @@ args = parser.parse_args()
 # Let us create a connection first
 rpc_connection = AuthServiceProxy("http://%s:%s@%s"%(args.rpc_user[0], args.rpc_password[0], args.rpc_endpoint[0]))
 
-# How much time needs to pass before refetching the template / target and looking for new unspent outputs
+# Here, we configure how much time needs to pass before refetching the template / target and looking for new unspent outputs
 MAX_INNER_LOOP_TIME = 5*60 # 5 minutes
 HEART_BEAT_TIME = 10 # print hashrate every 10 seconds
 
@@ -62,10 +62,48 @@ while 1==1:
 
     # First we must get the "GAS mining template".
     # It contains the BitcoinPy payload (basically the mine transaction in its raw form, pre-prepared with your address as the receipient) as well as the current difficulty
-    # All you will need to do from now on is to shuffle the late 16 bytes of the template (which are the nonce) until you find a transaction hash that meets the above target requirement
     template = get_template(rpc_connection, args.address[0])
     binarypayload = binascii.unhexlify(template['transaction'])
     numericaltarget = int(template['target'], 16)
+
+#############################################################################################################
+# IMPORTANT INFORMATION
+#############################################################################################################
+#
+# The RPC command looks like this:
+# minegastemplate 2N9XtqgAeDHmM4Zq32YhzaUtP7a8nSDUDZA
+
+# The GAS mining template answer looks like this:
+# {"transaction": "030000000000000000000000000000000023324e3958747167416544486d4d345a71333259687a615574503761386e534455445a411055555555555555555555555555555555",
+#  "target": "0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"}
+#
+# Transaction is a hex encoded serialized version of this C structure:
+#    SerializablePayload pl;
+#    std::vector<char> data;
+#    for (int i = 0; i < 16; ++i)
+#        data.push_back(0x55);
+#    pl.data = std::string(data.begin(), data.end());
+#    pl.gaslimit = 0;
+#    pl.value = 0;
+#    pl.command = CMD_MINER; // 0x03
+#    pl.receiver = "2N9XtqgAeDHmM4Zq32YhzaUtP7a8nSDUDZA";
+#
+# ... with
+#
+# class SerializablePayload
+# {
+# public:
+#    ...
+#    std::string data;
+#    uint8_t command;
+#    uint64_t value;
+#    uint64_t gaslimit;
+#    std::string receiver;
+#    ... }
+#
+# The last 16 bytes, here prefilled with all 0x55, can be varied by the miner
+#############################################################################################################
+
 
     # since the GAS mining hash depends on all "input prevout's" of the transaction, we have to decide which inputs we want to pull in early on
     # Note: It does not depend on the inputs, but the input's prevouts! Making the signature of those inputs entirely irrelevant!
@@ -85,7 +123,6 @@ while 1==1:
     voutpreface = binascii.unhexlify(used_vout["txid"])[::-1] + used_vout["vout"].to_bytes(4, byteorder = 'big') 
     midstateinput = binarypayload[0:28]
     secondpart = binarypayload[28:]
-
 
     # Now, we have the mining template and one output that can most likely cover the BTC relay fees. We can now start an inner loop that will
     # either run for MAX_INNER_LOOP_TIME or until a valid hash was found and submitted
@@ -109,6 +146,45 @@ while 1==1:
             hrate = float(hashes) / float(time_since_heartbeat)
             print("[" + str(datetime.datetime.now().time()) + "]","hashrate over the last",round(time_since_heartbeat,2),"seconds:",readable_hashrate(hrate))
             hashes = 0
+
+#############################################################################################################
+# IMPORTANT INFORMATION
+#############################################################################################################
+#
+# The hash algorithm is a simple double SHA256d.
+# Here is what needs to be hashed to obtain the correct HASH
+#
+# For each input, or its COutPoint) (in this miner, we only have one) we have to write this into the hasher
+# - TXID, in binary (not hex) form, and reversed.
+# - VOUT, as big endian 32 bit integer
+#
+# Then, at the end, we hash in the entire "transaction" from above with the last 16 byte changed to whatever you want
+# You can optimize a lot, when you take the first 64 byte (which are all static) and calculate a reusable midstate
+# All what is left is less than one full SHA256 block, which can be hashed quite efficiently.
+#
+#
+#############################################################################################################
+#
+# Here, a specific example of what bytes are hashed for one specific example
+#
+# 0799df867cb89d9b60be55419dcab1902383996b5ff0747f9c73517d1f4ccf31 <--- TXID (reversed), transaction was 0x31cf4c1f....
+# 00000000 <--- big endian VOUT = 0
+# 03 <--- Part of "transaction" from the "template" / means command type 0x03 = MINING
+# 0000000000000000 <---- Part of "transaction" from the "template" / value transmitted, 64bit, must be 0 for mining
+# 0000000000000000 <---- Part of "transaction" from the "template" / gaslimit to pay, 64bit, must be 0 for mining
+# 23 <---- Part of "transaction" from the "template" / length of string receipient address
+# 324e3958747167416544486d4d345a71333259687a615574503761386e534455445a41 <---- Part of "transaction" from the "template" / receipient address raw bytes
+# 10 <--- Part of "transaction" from the "template" / Length of "rest", i.e., the nonce payload. Here 0x10 = 16
+# 5795ef9cdec2ef93c34998d824d73b5f <---- OUR RANDOM 16 BYTES
+#
+# AND THIS IS THE HASH THAT SHOULD COME OUT FOR THE EXAMPLE ABOVE
+#
+# 2018-12-24T12:14:49Z We saw a GAS-MINER transaction, credited 100000000000 + 0 (fees) satoshis in BTCGAS and sent to 2N9XtqgAeDHmM4Zq32YhzaUtP7a8nSDUDZA
+# 2018-12-24T12:14:49Z | preupdate wallet transaction; old debit: 0, new debit: 0, old credit: 0, new credit: 100000000000
+# 2018-12-24T12:14:49Z Hash was: 0000ceaa447004633363266352782fa80326e67e127f7458a5dfa4d7654d7356 [target 0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff]
+#############################################################################################################
+
+
 
         # create hasher with midstate
         hashround1 = SHA256()
