06f53afaa3b7f8a99f1e1b4698cd693c962b716e
peepingtom@bitcoinpy.io <Peeping Tom>
2019-01-17 14:23:06 +1000

    other quickwin

--- quickwin.sh
+++ quickwin.sh
@@ -1,1 +1,1 @@
-./miner --rpc_endpoint 127.0.0.1:19001 --rpc_user admin1 --rpc_password 123 -a 2NAcWhPYPCyaBkmJyK6k7DYKBGVyCzakP8z
+./miner.pyx --rpc_endpoint 127.0.0.1:19001 --rpc_user admin1 --rpc_password 123 -a miv9onxgzXEuwc3TwN8aYv1nCpoygz8KhE
